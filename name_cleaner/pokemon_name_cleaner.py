fb047c20a1a1c0de5b67b814809808e3cc02d51a
Colin <164561623+cesmit27@users.noreply.github.com>
2024-05-27 16:19:22 -0400

    Create pokemon_name_cleaner.py

---
+++ name_cleaner/pokemon_name_cleaner.py
@@ -0,0 +1,12 @@
+pokemon_input = """
+Totodile (Pokémon)
+Croconaw (Pokémon)
+Feraligatr (Pokémon)
+"""
+
+pokemon_list = pokemon_input.strip().split('\n')
+
+
+cleaned_pokemon_list = ['"' + pokemon.replace(" (Pokémon)", "") + '"' for pokemon in pokemon_list]
+
+print(", ".join(cleaned_pokemon_list))
